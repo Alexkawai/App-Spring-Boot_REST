882d2104bab4d643aacf1f939ed299e4fd25c7c3
Alexkawai <leha.iuganov@yandex.ru>
2021-07-26 18:07:57 +0300

    bootstrap

--- src/main/java/com/example/service/ServiceImpl.java
+++ src/main/java/com/example/service/ServiceImpl.java
@@ -1,7 +1,6 @@
 package com.example.service;
 
 import com.example.dao.Dao;
-import com.example.dao.UserRepository;
 import com.example.model.Role;
 import com.example.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.transaction.Transactional;
 import java.util.List;
-import java.util.Optional;
 import java.util.Set;
 
 @org.springframework.stereotype.Service
--- src/main/java/com/example/service/UserDetailsServiceImpl.java
+++ src/main/java/com/example/service/UserDetailsServiceImpl.java
@@ -2,19 +2,11 @@ package com.example.service;
 
 
 import com.example.dao.Dao;
-import com.example.dao.UserRepository;
-import com.example.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-
-import java.util.Collection;
 
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
